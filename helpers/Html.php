21396d220e271c4efa895f04f8ba03465569a108
drodata <drodata@foxmail.com>
2016-09-10 10:38:45 +0800

    enh: Html, enable `visible` in Html::a()

--- helpers/Html.php
+++ helpers/Html.php
@@ -1,7 +1,9 @@
 <?php
 namespace drodata\helpers;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 use yii\bootstrap\BaseHtml;
+
 /**
  * customize icon() method, with font awesome enabled
  *
@@ -16,5 +18,29 @@ class Html extends BaseHtml
         $classPrefix = ArrayHelper::remove($options, 'prefix', 'fa fa-');
         static::addCssClass($options, $classPrefix . $name);
         return static::tag($tag, '', $options);
+    }
+
+    /**
+     * Add the special `visible` attribute
+     */
+    public static function a($text, $url = null, $options = [])
+    {
+        if ($url !== null) {
+            $options['href'] = Url::to($url);
+        }
+        $visible = ArrayHelper::remove($options, 'visible', true);
+        return $visible ? static::tag('a', $text, $options) : '';
+    }
+
+    /**
+     * Add the special `visible` attribute
+     */
+    public static function button($content = 'Button', $options = [])
+    {
+        if (!isset($options['type'])) {
+            $options['type'] = 'button';
+        }
+        $visible = ArrayHelper::remove($options, 'visible', true);
+        return $visible ? static::tag('button', $content, $options) : '';
     }
 }
