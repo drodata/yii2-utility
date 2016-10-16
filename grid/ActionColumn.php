b049bc20a862ee515f77350146da5b4f711887f9
drodata <drodata@foxmail.com>
2016-10-16 15:29:29 +0800

    add: ActionColumn using font awesome

---
+++ grid/ActionColumn.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace drodata\grid;
+
+use Yii;
+use yii\helpers\Url;
+use drodata\helpers\Html;
+
+class ActionColumn extends yii\grid\ActionColumn
+{
+    /**
+     * Migrate glyphicon to fant awesome
+     *
+     * Initializes the default button rendering callbacks.
+     */
+    protected function initDefaultButtons()
+    {
+        if (!isset($this->buttons['view'])) {
+            $this->buttons['view'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('yii', 'View'),
+                    'aria-label' => Yii::t('yii', 'View'),
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a(Html::icon('eye'), $url, $options);
+            };
+        }
+        if (!isset($this->buttons['update'])) {
+            $this->buttons['update'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('yii', 'Update'),
+                    'aria-label' => Yii::t('yii', 'Update'),
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a(Html::icon('pencil'), $url, $options);
+            };
+        }
+        if (!isset($this->buttons['delete'])) {
+            $this->buttons['delete'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('yii', 'Delete'),
+                    'aria-label' => Yii::t('yii', 'Delete'),
+                    'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                    'data-method' => 'post',
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a(Html::icon('trash'), $url, $options);
+            };
+        }
+    }
+}
