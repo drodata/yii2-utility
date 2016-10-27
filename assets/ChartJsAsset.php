61f533fc0a693f6338d4d53193c4c7a2979addd8
drodata <drodata@foxmail.com>
2016-10-27 20:45:05 +0800

    add Chart.js asset

---
+++ assets/ChartJsAsset.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Asset for http://www.chartjs.org/ 
+ */
+namespace drodata\assets;
+use yii\web\AssetBundle;
+
+class ChartJsAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/chartjs/dist';
+    public $js = [
+        'Chart.bundle.min.js'
+    ];
+}
