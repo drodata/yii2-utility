025d5e2ab55dcf1adce6ba85ebec3773517da4b3
drodata <drodata@foxmail.com>
2018-04-25 15:16:18 +0800

    Enh: add icon() to LookupBehavior #39

--- behaviors/LookupBehavior.php
+++ behaviors/LookupBehavior.php
@@ -3,6 +3,7 @@
 namespace drodata\behaviors;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 use yii\base\InvalidConfigException;
 use drodata\models\Lookup;
 use drodata\helpers\Html;
@@ -45,6 +46,35 @@ class LookupBehavior extends \yii\base\Behavior
     public $labelMap = [];
 
     /**
+     * @var array 字体图标配置数组。格式如下：
+     *
+     * ```php
+     * [
+     *     'status' => ['order-status', 'icon-name', [
+     *         self::STATUS_UNPAID => 'danger',
+     *         self::STATUS_PAID => 'success',
+     *     ]],
+     * ]
+     * ```
+     * 
+     * 更个性化的属性形式:
+     * 
+     * ```php
+     * [
+     *     'status' => ['order-status', 'rmb', [
+     *         self::STATUS_UNPAID => [
+     *             'icon' => 'dollar',
+     *             'color' => 'warning',
+     *             'tooltip' => 'U',
+     *         ],
+     *     ]],
+     * ]
+     * ```
+     *
+     */
+    public $iconMap = [];
+
+    /**
      * 从 labelMap 中获取指定属性的配置信息
      */
     protected function lookupConfig($attribute)
@@ -53,6 +83,20 @@ class LookupBehavior extends \yii\base\Behavior
 
         if (empty($configs)) {
             throw new InvalidConfigException("Please config '$attribute' in the `labelMap` option first");
+        }
+
+        return $configs;
+    }
+
+    /**
+     * 从 iconMap 中获取指定属性的配置信息
+     */
+    protected function iconConfig($attribute)
+    {
+        $configs = $this->iconMap[$attribute];
+
+        if (empty($configs)) {
+            throw new InvalidConfigException("Please config '$attribute' in the `iconMap` option first");
         }
 
         return $configs;
@@ -83,9 +127,55 @@ class LookupBehavior extends \yii\base\Behavior
     {
         list($type, $colorMap) = $this->lookupConfig($attribute);
         $code = $this->owner->$attribute;
+        $color = $colorMap[$code];
 
-        return Html::tag('span', $this->lookup($attribute), [
-            'class' => "label label-{$colorMap[$code]}",
+        if (empty($code) || empty($color)) {
+            return '';
+        }
+
+        if (is_array($color)) {
+            $labelColor = ArrayHelper::remove($color, 'color', 'default');
+            $labelText = ArrayHelper::remove($color, 'text', $this->lookup($attribute));
+        } elseif (is_string($color)) {
+            $labelColor = $color;
+            $labelText = $this->lookup($attribute);
+        }
+
+        return Html::tag('span', $labelText, [
+            'class' => "label label-{$labelColor}",
+        ]);
+    }
+
+    /**
+     * 显示属性对应的字体图标
+     *
+     * @param string $attribute 属性列名称
+     * @return string
+     *
+     */
+    public function icon($attribute)
+    {
+        list($type, $iconName, $colorMap) = $this->iconConfig($attribute);
+
+        $tooltip = $this->lookup($attribute);
+        $code = $this->owner->$attribute;
+        $color = $colorMap[$code];
+
+        if (empty($code) || empty($color)) {
+            return '';
+        }
+
+        if (is_array($color)) {
+            $iconName = ArrayHelper::remove($color, 'icon', $iconName);
+            $iconColor = ArrayHelper::remove($color, 'color', 'default');
+            $iconTooltip = ArrayHelper::remove($color, 'tooltip', $this->lookup($attribute));
+        } elseif (is_string($color)) {
+            $iconColor = $color;
+            $iconTooltip = $this->lookup($attribute);
+        }
+
+        return Html::tooltipIcon($iconTooltip, $iconName, [
+            'class' => "text-{$iconColor}",
         ]);
     }
 }
