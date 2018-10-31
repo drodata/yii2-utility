3f16bdaf2af13e5f4711bbae4f4abd260dbdd5c2
drodata <drodata@foxmail.com>
2018-10-31 16:55:52 +0800

    微调 CurrencyController

--- controllers/CurrencyController.php
+++ controllers/CurrencyController.php
@@ -30,20 +30,6 @@ class CurrencyController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'actions' => ['xxx'], // 禁止访问的放在最前面
-                        'allow' => false,
-                    ],
-                    [
-                        //'actions' => ['create', 'view', 'update', 'delete'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -56,13 +42,13 @@ class CurrencyController extends Controller
     /**
      * Finds the Currency model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param string $id
+     * @param string $code
      * @return Currency the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
+    protected function findModel($code)
     {
-        if (($model = Currency::findOne($id)) !== null) {
+        if (($model = Currency::findOne($code)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
@@ -84,46 +70,6 @@ class CurrencyController extends Controller
     }
 
     /**
-     * Displays a single Currency model.
-     * @param string $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * View a record in modal
-     *
-     * @param string $id
-     */
-    public function actionModalView($id)
-    {
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-
-        return $this->renderPartial('modal-view', [
-            'model' => $this->findModel($id),
-        ]);
-
-    }
-
-    /**
-     * 在 Modal 内高级搜索
-     */
-    public function actionModalSearch()
-    {
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-
-        return $this->renderPartial('modal-search', [
-            'model' => new CurrencySearch(),
-        ]);
-
-    }
-
-    /**
      * Creates a new Currency model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
@@ -134,8 +80,7 @@ class CurrencyController extends Controller
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             Yii::$app->session->setFlash('success', '新记录已创建');
-            return $this->redirect('index');
-            //return $this->redirect(['view', 'id' => $model->code]);
+            return $this->goHome();
         }
 
         return $this->render('create', [
@@ -144,24 +89,14 @@ class CurrencyController extends Controller
     }
 
     /**
-     * AJAX 提交表单
-     */
-    public function actionAjaxSubmit()
-    {
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-
-        return Currency::ajaxSubmit($_POST);
-    }
-
-    /**
      * Updates an existing Currency model.
      * If update is successful, the browser will be redirected to the 'view' page.
-     * @param string $id
+     * @param string $code
      * @return mixed
      */
-    public function actionUpdate($id)
+    public function actionUpdate($code)
     {
-        $model = $this->findModel($id);
+        $model = $this->findModel($code);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             Yii::$app->session->setFlash('success', '修改已保存');
@@ -176,42 +111,14 @@ class CurrencyController extends Controller
     /**
      * Deletes an existing Currency model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param string $id
+     * @param string $code
      * @return mixed
      */
-    public function actionDelete($id)
+    public function actionDelete($code)
     {
-        $this->findModel($id)->delete();
+        $this->findModel($code)->delete();
         Yii::$app->session->setFlash('success', '已删除');
 
         return $this->redirect(Yii::$app->request->referrer);
-    }
-
-    /**
-     * 为 Currency 模型上传附件 
-     * @param string $id
-     * @return mixed
-     */
-    public function actionUpload($id)
-    {
-        $model = $this->findModel($id);
-        // 根据需要修改场景值
-        $common = new CommonForm(['scenario' => CommonForm::SCENARIO_XXX]);
-
-        if ($common->load(Yii::$app->request->post())) {
-            $common->images = UploadedFile::getInstances($common, 'images');
-            if ($common->validate()) {
-                $model->on(Currency::EVENT_UPLOAD, [$model, 'insertImages'], $common->images);
-                $model->trigger(Currency::EVENT_UPLOAD);
-                Yii::$app->session->setFlash('success', '图片已上传。');
-
-                return $this->redirect('index');
-            }
-        }
-
-        return $this->render('upload', [
-            'model' => $model,
-            'common' => $common,
-        ]);
     }
 }
--- views/currency/_grid.php
+++ views/currency/_grid.php
@@ -8,47 +8,21 @@ use backend\models\Lookup;
 /* @var $searchModel drodata\models\CurrencySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-/**
- * 借助 'caption' 属性显示筛选数据累计金额
-if (empty(Yii::$app->request->get('CurrencySearch'))) {
-    $caption = '';
-} else {
-    $sum = 0;
-    foreach ($dataProvider->models as $model) {
-        $sum += $model->amount;
-    }
-    $badge = Html::tag('span', Yii::$app->formatter->asDecimal($sum), [
-        'class' => 'badge',
-    ]);
-    $caption = Html::tag('p', "筛选金额累计 $badge");
-}
- */
 echo GridView::widget([
     'dataProvider' => $dataProvider,
-    // 'caption' => $caption,
     'filterModel' => $searchModel,
     'columns' => [
-        // ['class' => 'yii\grid\SerialColumn'],
         'code',
         'name',
         'symbol',
         [
             'class' => 'drodata\grid\ActionColumn',
-            'template' => '{view} {update} {delete}',
+            'template' => '{update}',
             'contentOptions' => [
                 'style' => 'min-width:120px',
                 'class' => 'text-center',
             ],
             'buttons' => [
-                'view' => function ($url, $model, $key) {
-                    return $model->actionLink('view');
-                },
-                'update' => function ($url, $model, $key) {
-                    return $model->actionLink('update');
-                },
-                'delete' => function ($url, $model, $key) {
-                    return $model->actionLink('delete');
-                },
             ],
         ],
     ],
